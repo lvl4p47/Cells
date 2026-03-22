961d95a1ad0c7c1d804d6a276af6279cde22030e
банка <99590719+lvl4p47@users.noreply.github.com>
2026-03-22 19:24:44 +0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -6,5 +6,17 @@ if [ ! -d "build" ]; then
 fi
 
 cd build
+
+# Создаём папки genomes и screenshots внутри build
+if [ ! -d "genomes" ]; then
+    echo "Creating genomes directory ..."
+    mkdir genomes
+fi
+
+if [ ! -d "screenshots" ]; then
+    echo "Creating screenshots directory ..."
+    mkdir screenshots
+fi
+
 #cmake ..
 make -j3
